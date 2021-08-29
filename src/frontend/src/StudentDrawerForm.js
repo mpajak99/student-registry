308d338e06fc38dd188299d7a005c6019562a218
mpajak <mpajak_99@wp.pl>
2021-08-29 12:51:16 +0200

    Add notifications

--- src/frontend/src/StudentDrawerForm.js
+++ src/frontend/src/StudentDrawerForm.js
@@ -12,6 +12,7 @@ import {
 import {addNewStudent} from "./client";
 import {LoadingOutlined} from "@ant-design/icons";
 import {useState} from "react";
+import {successNotification, errorNotification} from "./Notification";
 
 const {Option} = Select;
 const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
@@ -27,6 +28,10 @@ function StudentDrawerForm({showDrawer, setShowDrawer, fetchStudents}) {
             .then(() => {
                 console.log("student added");
                 onCLose();
+                successNotification(
+                    "Student successfully added",
+                    `${student.name} was added to the system`
+                    );
                 fetchStudents();
             }).catch(err => {
                 console.log(err);
